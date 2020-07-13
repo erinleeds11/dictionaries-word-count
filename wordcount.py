b7f150c0206984bf0e316e06057ddae65bc2b6af
Erin Leeds <erin@erinleeds.com>
2020-07-13 18:48:16 +0000

    Tested with test.txt

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,18 @@
-# put your code here.
+def word_count(filename):
+    script = open(filename)
+
+    word_counts = {}
+    
+    for line in script:
+        lines = line.rstrip()
+        words = lines.split(" ")
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
+
+print(word_count("twain.txt"))
+
+
+
+
